21f68094b6676226b57537a21c086fd7cba54efc
Hlogimaepa <hlogimaepa80@gmail.com>
2023-02-16 15:12:40 +0200

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,12 @@
 
 /**
  * main - A program that prints a new line using the printf function
+ *
  *  Return: 0 (Success)
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
