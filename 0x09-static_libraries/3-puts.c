c3acc04456a8843d72d33f8fd1a3b64df00b1727
Hlogimaepa <hlogimaepa80@gmail.com>
2023-03-10 09:37:07 +0200

    clear

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void _puts(char *str);
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str);
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
